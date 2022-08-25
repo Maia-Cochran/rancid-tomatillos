55fe71c302a789d4a84e9db23c92b6b3832f4ccc
Thomas Fettig <thomasmfettig@gmail.com>
2022-08-25 17:26:53 -0600

    Add selectAMovie method

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,11 +17,23 @@ addMovies = (movies) => {
   this.setState({ movies: [...this.state.movies, movies] })
 }
 
+selectAMovie = (event) => {
+  let selectedMovie;
+  this.state.movies.forEach(movie => {
+    if (movie.id === parseInt(event.target.id)) {
+      selectedMovie = movie
+    }
+  })
+  console.log(selectedMovie.title)
+  return selectedMovie
+}
+
 render(){
     return (
         <main className = 'App'>
             <Header />
-            <MovieContainer movies={this.state.movies}/>     
+            <MovieContainer movies={this.state.movies} selectAMovie={this.selectAMovie}/>   
+            {/* {event.target.className.contains('movie-card') && <Modal id=event.target.id/>}   */}
         </main>
     )
   };
