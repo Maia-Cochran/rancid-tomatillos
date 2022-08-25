500f9ff5b35924699a059792b06c182439049102
Thomas Fettig <thomasmfettig@gmail.com>
2022-08-25 15:27:22 -0600

    Add movieContainer and Card components and some css for them

--- src/components/MovieContainer/MovieContainer.js
+++ src/components/MovieContainer/MovieContainer.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import Card from '../Card/Card'
 import './MovieContainer.css'
+import logo from '../../images/rancid-tomatillos.png'
 
 
-export default function MovieContainer() {
+const MovieContainer = ({ movies }) => {
+
+  const movieCards = movies.map(movie => {
+    return (
+      <Card
+        logo={logo}
+        posterImage={movie.poster_path}
+        rating={movie.average_rating.toFixed(1)}
+        key={movie.id}
+      />
+    )
+  })
+
   return (
-    <div className ="MovieContainer">
-        <Card />
+    <div className ="movie-container">
+      {movieCards}
     </div>
   )
 }
+
+
+export default MovieContainer
