135750121817f7a53f0d75367e80bbae8c9151a3
Maia Cochran <maia.cochran1@gmail.com>
2022-08-31 15:15:12 -0700

    Apply flex grid, refactor to figma wireframe layout

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -25,28 +25,22 @@ class SingleMovie extends Component {
     } else {
       return (
       <section className='movie-info'> 
-        <section className='home-btn-container'>
-          <NavLink to="/">
-            <button className='back-to-home-btn' alt='homeBtn'>🄧</button>
-          </NavLink>
-        </section>
-        <img className='modal-mini-poster' id={movie.id} src={movie.poster_path} alt='poster'/>
+        <NavLink to="/">
+          <button className='back-to-home-btn' alt='homeBtn'>X</button>
+        </NavLink>
         <section className='movie-details-container'>
+          <img className='modal-mini-poster' id={movie.id} src={movie.poster_path} alt='poster'/>
           <h2 className='modal-title'>{movie.title}</h2>
           <p className='modal-release-date'><b>Release Date:</b> {dayjs(movie.release_date).format('MMM. D, YYYY')}</p>
           <p className='genres'><b>Related Genres:</b> {(movie.genres).join(', ')}</p>
           <p className='runtime'><b>Runtime:</b> {movie.runtime} minutes</p>
           <p className='budget'><b>Budget:</b> ${(movie.budget).toLocaleString('en-US')}</p>
           <p className='revenue'><b>Box Office Revenue:</b> ${(movie.revenue).toLocaleString('en-US')}</p>
-          <section className='overview-container'>
-            <p className='modal-overview'><b>Overview:</b> {movie.overview}</p>
-          </section>
+          <p className='modal-overview'><b>Overview:</b> {movie.overview}</p>
         </section>
-        <section className='rating-tagline-container'>
-          <p className='modal-rating'>{Math.round(movie.average_rating)} STARS</p>
+          <p className='modal-rating'><i><b>Rating: </b>{(movie.average_rating).toFixed(1)}</i></p>
           <p className='tagline'><i>"{movie.tagline}"</i></p>
-        </section>
-        <img className='modal-back-drop' id={movie.id} src={movie.backdrop_path} alt='poster'/>
+          <img className='modal-back-drop' id={movie.id} src={movie.backdrop_path} alt='poster'/>
       </section>
       )
     }
