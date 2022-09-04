08a2ad0c1ac826a0f5996aee4943549be708c051
Chantal Goethals <chantalgoethals@gmail.com>
2022-09-04 13:41:31 -0600

    Feat: format 404 response page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from 'react';
 import Status from '../Status/Status';
 import './NotFound.css';
+import splat from '../../images/404-splat.svg';
+
+
+
+
 
 const NotFound = () => {
     return (
       <Status code={404}>
-        <div>
+        <div className='error-wrapper'>
           <h1>Sorry, can't find that.</h1>
+          <h2>404</h2>
+          <img className="splat" src={splat} />
         </div>
       </Status>
     );
