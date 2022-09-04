85821306bc3ec7d348bfb18a89effd9ca8b4b92e
Maia Cochran <maia.cochran1@gmail.com>
2022-09-04 13:04:30 -0700

    Add tests for Not Found component with Cypress

--- cypress/e2e/error_handling.cy.js
+++ cypress/e2e/error_handling.cy.js
@@ -1,5 +1,12 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Error handling', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should display the Not Found component', () => {
+    cy.visit('http://localhost:3000/NotFound')
+    cy.get('h1')
+    .should('be.visible')
+    .contains(`Sorry, can't find that.`)
   })
 })
